796d913e154abe390e66d75b4fd24e5f6258f80d
Eko Saputra <Ekosaputra.t.i@gmail.com>
2024-08-07 13:41:17 +0700

    conf vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '203.145.34.172',
+    host: '0.0.0.0',
     port: 3000
   }
 })
