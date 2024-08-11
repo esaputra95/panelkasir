f392084a588f2f06c6c3f58e002c690d4283f5c7
Eko Saputra <Ekosaputra.t.i@gmail.com>
2024-08-11 15:09:58 +0700

    dashboard

--- src/pages/admins/masters/dashboards/dashboard.tsx
+++ src/pages/admins/masters/dashboards/dashboard.tsx
@@ -12,7 +12,7 @@ const Dashboard = () => {
   } = useDashboard()
   return (
     <div className="w-full">
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
         <CardDashboard
           bgColor="blue"
           label="Penjualan"
